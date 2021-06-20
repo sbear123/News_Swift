5d3b682fb143cf4c0fdce6afbf13b0dea1189133
sbear123 <sbear123@dgsw.hs.kr>
2021-06-21 08:08:40 +0900

    Make Project

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by 박지현 on 2021/06/20.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
